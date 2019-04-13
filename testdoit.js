4aa9cd646642b728efe8428f4211baff2ef393b9
Yudai <hidamaryclover@gmail.com>
2019-04-13 20:27:13 +0900

    [add/update]html/css 0413_01

--- testdoit.js
+++ testdoit.js
@@ -3,3 +3,21 @@ $('#input').click(function () {
     $('#output').html("わたしは" + str + "です");
 })
 
+// header animation
+// 変数「heroBottom」にメイン画像エリアの高さを入れています。画像ですので、jQueryが読み込まれてすぐに変数に高さを入れようとすると正常な高さが取得できません。
+// ですので、スクロール時に高さを変数に入れています
+var _window = $(window),
+    _header = $('.header'),
+    heroBottom;
+
+_window.on('scroll', function () {
+    heroBottom = $('.heroBottom').height();
+    if (_window.scrollTop() > heroBottom) {
+        _header.addClass('transform');
+    } else {
+        _header.removeClass('transform');
+    }
+});
+
+_window.trigger('scroll');
+
