1ea16a0025b2ef8596a1bfd9a03b1dd6cc38c482
Yudai <hidamaryclover@gmail.com>
2019-04-15 22:15:56 +0900

    viewport test

--- testdoit.js
+++ testdoit.js
@@ -19,3 +19,32 @@ _window.on('scroll', function () {
 
 _window.trigger('scroll');
 
+//cat_imgをクリックしたら、中央に表示
+var left = ($(window).width() - $('#largeImg').width()) / 2;
+var top = $(window).scrollTop() + 30 + 'px';
+
+$('#catImg').bind('click', function (e) {
+    e.preventDefault();
+
+    // 暗幕
+    $('#cover').css({
+        'width': $(window).width(),
+        'height': $(window).height()
+    }).show();
+
+    // 猫
+    $('#largeImg').css({
+        'position': 'absolute',
+        'left': ($(window).width() - $('#largeImg').width()) / 2,
+        'top': $(window).scrollTop() + 300 + 'px',
+    })
+        .show();
+
+    // 猫を閉じる
+    $('#largeImg, #cover').bind('click', function () {
+        $('#largeImg').fadeOut('slow', function () {
+            $('#cover').hide();
+        })
+    })
+});
+
